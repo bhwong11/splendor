6ff194a1c985fa0863fd07522d4ef92dbe1280a7
Bryant Wong <bryanthwong@gmail.com>
2023-09-19 21:19:10 -0400

    added placehodler grid

---
+++ splendor-frontend/src/app/rooms/[roomNumber]/play/cardsGrid.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { useEffect,useState } from "react";
+import { socketInitializeRoom } from "@/socket";
+import { useUserStore } from "@/zustand";
+import { useRouter } from "next/navigation";
+
+let socket;
+
+const CardsGrid = ({params})=>{
+  console.log(params.roomNumber)
+  const [users,setUsers] = useState([])
+  const username = useUserStore(state=>state.username)
+  const router = useRouter()
+  useEffect(()=>{
+    console.log('username!',username)
+    if(!username){
+      router.push('/')
+    }
+    socket = socketInitializeRoom(params.roomNumber,username)
+    console.log('soc',socket)
+    if(socket){
+      socket.on('user-joined',data=>{
+        console.log('user=join',data)
+        setUsers(data)
+      })
+      socket.on('user-left',data=>{
+        console.log('user-left',data)
+        setUsers(data)
+      })
+    }
+  },[])
+
+  return (
+      username && (
+      <div>
+          <h1>Room</h1>
+          <h3>{params.roomNumber}</h3>
+          {users?.map(user=>(
+            <div key={user.username}>
+            <span>{user.username}</span>
+            <span>{user.active?"ACTIVE":"NOT ACTIVE"}</span>
+            </div>
+          ))}
+      </div>)
+  )
+}
+
+export default CardsGrid
